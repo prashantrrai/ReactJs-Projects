3464c3a545f54ec2d31d5d2628087a6a428e1104
Prashant Rai <19ce073@gardividyapith.ac.in>
2023-10-09 02:55:12 +0530

    React E-book started to practice]

---
+++ ebook/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className={styles.head}>Hello, React!</h1>
+    </div>
+  );
+};
+
+
+export default App
