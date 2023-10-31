09f9ef50b64cf300b6f1911923c37821f2f608bb
Prashant Rai <19ce073@gardividyapith.ac.in>
2023-11-01 00:20:27 +0530

    Tic-Tac-Toe (Board, Square, State, Props done)

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Square from './Components/Square'
+import Board from './Components/Board'
+
+const App = () => {
+  return (
+    <>
+      <Board/>
+    </>
+  )
+}
+
+export default App
